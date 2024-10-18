717c08e1d2b4a5f4183b34a349302db4c81a2d2b
ji_steven11 <steven.ji@epfl.ch>
2024-10-18 16:02:08 +0200

    Version Initiale

---
+++ src/main/java/ch/epfl/cs107/Challenge.java
@@ -0,0 +1,29 @@
+package ch.epfl.cs107;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <b>06</b> Provided for you to attempt the challenge in
+ *
+ * @author Mehdi ALAOUI (mehdi.alaoui@epfl.ch)
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Challenge {
+
+    // DO NOT CHANGE THIS, MORE ON THAT ON WEEK 7
+    private Challenge(){}
+
+    // ============================================================================================
+    // ======================================== CHALLENGE =========================================
+    // ============================================================================================
+
+    /**
+     * Solves the challenge
+     *
+     * @return the flag in the format FLAG{********}
+     */
+    public static String challenge(){
+        return Helper.fail("NOT IMPLEMENTED");
+    }
+}
